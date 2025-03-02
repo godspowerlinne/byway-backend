327822f8e148cae142f4b90f5291b6d46402bed1
godspowerlinne <amriccygodspea@gmail.com>
2025-03-03 00:33:28 +0100

    feat: Implement validation rules for user signup and signin with express-validator

---
+++ auth/validator.js
@@ -0,0 +1,72 @@
+const { body, validationResult } = require("express-validator");
+
+// signup validation
+const signupValidationRules = [
+    body("firstname")
+        .trim()
+        .notEmpty()
+        .withMessage("First name is required")
+        .isLength({ min: 2, max: 30 })
+        .withMessage("First name must be between 2 and 30 characters"),
+    body("lastname")
+        .trim()
+        .notEmpty()
+        .withMessage("Last name is required")
+        .isLength({ min: 2, max: 30 })
+        .withMessage("Last name must be between 2 and 30 characters"),
+
+    body("username")
+        .trim()
+        .notEmpty()
+        .withMessage("Username is required")
+        .isAlphanumeric()
+        .withMessage("Username must be alphanumeric")
+        .isLength({ min: 3, max: 30 })
+        .withMessage("Username must be between 3 and 30 characters"),
+    body("email")
+        .trim()
+        .notEmpty()
+        .withMessage("Email is required")
+        .isEmail()
+        .withMessage("Invalid email address"),
+
+    body("password")
+        .trim()
+        .notEmpty()
+        .withMessage("Password is required")
+        .isLength({ min: 8 })
+        .withMessage("Password must be at least 8 characters")
+        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{9,}$/)
+  .withMessage("Password must contain at least one uppercase letter, one lowercase letter, and one digit");
+];
+
+// sign in validation
+const signinValidationRules = [
+    body("email")
+        .trim()
+        .notEmpty()
+        .withMessage("Email is required")
+        .isEmail()
+        .withMessage("Invalid email address"),
+    body("username").trim().notEmpty().withMessage("Username Required"),
+
+    body("password").trim().notEmpty().withMessage("Password Required"),
+];
+
+//middleware to handle validation errors
+const validate = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+                success: false,
+                errors: errors.array().map((err) => err.msg) 
+            });
+    }
+    next();
+};
+
+module.exports = {
+    signupValidationRules,
+    signinValidationRules,
+    validate,
+};
