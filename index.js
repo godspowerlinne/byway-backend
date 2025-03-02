377354e951f7bd4cfa684724395a71a942e02875
godspowerlinne <amriccygodspea@gmail.com>
2025-03-02 19:56:32 +0100

    feat: Configure Express server with MongoDB, CORS and rate limiting

---
+++ index.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+
+const app = express();
+
+app.use(cors(
+    {
+        origin: "*", // Allow requests from any origin
+        Credentials: true
+    }
+));
+
+app.use(express.json());
+
+const AuthLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per windowMs
+    message: "Too many requests, please try again later." 
+});
+
+app.use(AuthLimiter);
+
+const url = process.env.MONGODB_URL; 
+const options = { serverSelectionTimeoutMS: 30000, connectTimeoutMS: 5000, }; // Set connection timeout to 30 seconds
+
+// Connect to MongoDB before starting the server 
+const connectDB = async () => {
+    try {
+        await mongoose.connect(url, options);
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.log('Error connecting to MongoDB:', error);
+        process.exit(1); // Exit the process if connection fails
+    }
+
+    const port = process.env.PORT || 5000;
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+
+}
+connectDB();
+
+// Export the app for Deployment
+module.exports = app;
