159d51c63f8ac1ef9c3534e6e62e39643ce13355
godspowerlinne <amriccygodspea@gmail.com>
2025-03-17 16:28:56 +0100

    feat: Extend User model with roles, profiles, and course relations
    - Introduced user roles (admin, student, instructor) and profile details.
    - Implemented course tracking with enrolledCourses, createdCourses, and wishlist arrays.
    - Added social link fields and helper methods.

--- models/User.js
+++ models/User.js
@@ -1,36 +1,86 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
     firstname: {
         type: String,
-        required: true
+        required: true,
+        trim: true,
     },
     lastname: {
         type: String,
-        required: true
+        required: true,
+        trim: true,
     },
     username: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        trim: true,
     },
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
     },
     password: {
         type: String,
         required: true,
-        minlength: 8
+        minlength: 8,
     },
-},  { timestamps: true });
+    role: {
+        type: String,
+        enum: ["admin", "student", "instructor"],
+        default: "student",
+    },
+    profileImage: {
+        type: String,
+        default: "default.jpg",
+    },
+    bio: {
+        type: String,
+        default: "",
+    },
+    title: {
+        type: String,
+        default: "",
+    },
+    experience: {
+        type: Number,
+        default: "",
+    },
+    socialLinks: {
+        facebook: String,
+        twitter: String,
+        linkedin: String,
+        github: String,
+        website: String,
+    },
+
+    // Some things related to the users to track the courses they have enrolled in, or that they are a tutor of
+    enrolledCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+    }, ],
+
+    // tracks the users created course
+    createdCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+    }, ],
+
+    wishlist: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+    }, ],
+}, {
+    timestamps: true
+});
 
 // Hash the password before saving it to the database
-userSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) return next();
-    this.password = await bcrypt.hash(this.password, 12); 
+userSchema.pre("save", async function(next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 12);
     next();
 });
 
@@ -38,7 +88,16 @@ userSchema.pre('save', async function(next) {
 userSchema.methods.comparePassword = async function(enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
+// Method to get fullname
+userSchema.methods.getFullName = function() {
+    return `${this.firstname} ${this.lastname}`;
+};
 
-const User = mongoose.model('User', userSchema);
+// to get the number of courses registered for a user (instructor)
+userSchema.virtual("courseCount").get(function() {
+    return this.createdCourses?.length || 0;
+});
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
