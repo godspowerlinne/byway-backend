bee2ebba8326bc5399dad3b14f856adebf23c655
godspowerlinne <amriccygodspea@gmail.com>
2025-04-10 17:14:28 +0100

    feat: Enhance errorHandler with additional features
    Implemented a handle for mongoose validation error and duplicate errors
    Jwt errors and Default server error

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,12 +1,45 @@
 const errorHandler = (err, req, res, next) => {
-    console.log("Error", err);
-  
-    //default error message and status code
-    const statusCode = err.status || 500;
-    const message = err.message || "Internal server error";
-  
-    //send response
-    res.status(statusCode).json({ success: false, message });
-  };
-  
-  module.exports = errorHandler;
+	console.error(err.stack);
+
+	// Mongoose validation error
+	if (err.name === "ValidationError") {
+		const errors = Object.values(err.errors).map((val) => val.message);
+		return res.status(400).json({
+			success: false,
+			message: "Validation Error",
+			errors,
+		});
+	}
+
+	// Mongoose duplicate error
+	if (err.code === 11000) {
+		return res.status(400).json({
+			success: false,
+			message: "Duplicate field value entered",
+			field: Object.keys(err.keyValue)[0],
+		});
+	}
+
+	//JWT Errors
+	if (err.name === "JsonWebTokenError") {
+		return res.status(401).json({
+			success: false,
+			message: "Invalid Token",
+		});
+	}
+	if (err.name === "TokenExpiredError") {
+		return res.status(401).json({
+			success: false,
+			message: "Token Expired",
+		});
+	}
+
+	// Default to 500 server error
+	res.status(err.statusCode || 500).json({
+		success: false,
+		message: err.message || "Server Error",
+		error: process.env.NODE_ENV === "development" ? err : {},
+	});
+};
+
+module.exports = errorHandler;
